075457b7755d0f566d48108a69ef5535b94c9639
coderakr <139627558+coderakr@users.noreply.github.com>
2026-04-19 12:07:50 +0530

    auth ui add

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,20 +1,112 @@
+"use client";
+
+import { signUpSchema } from "@/app/schemas/auth";
+import { Button } from "@/components/ui/button";
 import {
+  Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { startTransition } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
 
 export default function SignUpPage() {
+  const form = useForm<z.input<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(data: z.input<typeof signUpSchema>) {
+    console.log("form submit...");
+  }
+
   return (
     <div>
-      <CardHeader>
-        <CardTitle>Sign up</CardTitle>
-        <CardDescription>Create an account to get started.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form></form>
-      </CardContent>
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign up</CardTitle>
+          <CardDescription>Create an account to get started.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Controller
+                name="name"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field>
+                    <FieldLabel>Full Name:</FieldLabel>
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      placeholder="John Doe"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field>
+                    <FieldLabel>Email:</FieldLabel>
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      placeholder="john@example.com"
+                      type="email"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field>
+                    <FieldLabel>Password:</FieldLabel>
+                    <Input
+                      aria-invalid={fieldState.invalid}
+                      placeholder="••••••••"
+                      type="password"
+                      {...field}
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Button>Sign up</Button>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
